fcad5d2df623857696de9bbea841ac4b5fc474c6
JCChiou <rex2763@gmail.com>
2020-05-01 21:29:44 +0800

    add retry count

--- no_guess.py
+++ no_guess.py
@@ -2,12 +2,15 @@
 #讓使用者重複輸入數字去猜
 import random
 r = random.randint(0, 100)
-print('0~100,數字猜猜看')
-while True:
+u_guess = 0
+retry_cnt = 0
+print('1~100,數字猜猜看')
+while u_guess != r:
+	retry_cnt = retry_cnt + 1
 	u_guess = int(input('請猜!'))
+	u_guess = int(u_guess)
 	if u_guess == r:
-		print('恭喜猜對了')
-		break
+		print('恭喜猜對了,你共猜了', retry_cnt ,'次')		
 	else:
 		if u_guess < r:
 			print('比答案小')
